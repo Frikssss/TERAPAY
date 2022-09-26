34903f7d0ac1e7d0037c0bd7bfca8cc2e36cdf27
Théo Frickert <97591760+Frikssss@users.noreply.github.com>
2022-09-26 14:31:27 +0200

    Delete TeraPay-main/tera_pay_v2 directory

--- TeraPay-main/tera_pay_v2/App.js
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, ScrollView, Text, View, Button, TextInput, Image, TouchableHighlight, TouchableOpacity } from 'react-native';
-import Navigation from './Components/Navigation/Navigation';
-
-export default function App() {
-  return (
-    <Navigation />
-  )
-};
--- TeraPay-main/tera_pay_v2/Components/Pages/HistoriqueList.js
+++
@@ -1,4 +0,0 @@
-import React from 'react-native';
-import { Component, Text, View } from 'react';
-
-class HistoriqueList extends Component {};
